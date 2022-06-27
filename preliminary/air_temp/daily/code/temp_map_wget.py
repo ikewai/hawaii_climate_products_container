1293a0ffb0d71d61aafe009ef781ef7cb837b27a
Sean Cleveland <sean.b.cleveland@gmail.com>
2022-06-27 12:58:14 -0600

    Update temp_map_wget.py

--- preliminary/air_temp/daily/code/temp_map_wget.py
+++ preliminary/air_temp/daily/code/temp_map_wget.py
@@ -49,10 +49,10 @@ if __name__=="__main__":
     #Air temp daily dependencies
     src_url = DEPEND_DIR + "dependencies.tar.gz"
     dest_path = LOCAL_DEPEND + "dependencies.tar.gz"
-    cmd = ["wget",src_url,"-P",dest_path]
+    cmd = ["wget",src_url]
     subprocess.call(cmd)
-    cmd = ["tar","-xvf",dest_path]
+    cmd = ["tar","-xvf","dependencies.tar.gz"]
     subprocess.call(cmd)
     dpath =  LOCAL_DEPEND +"/dependencies"
-    cmd =["cp", "-r", "dependencies",dpath] 
+    cmd =["cp", "-r", "dependencies/*",dpath] 
     subprocess.call(cmd)
